0176f411cb439fcf1468c5be791c9dbd5b7dcc9c
swapniljadhav96 <swapniljadhav3181@gmail.com>
2024-09-02 19:07:30 +0530

    add files

---
+++ Online-Job-Portal/login.php
@@ -0,0 +1,439 @@
+<!doctype html>
+<html lang="en">
+<?php 
+include 'constants/settings.php'; 
+include 'constants/check-login.php';
+?>
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Jobify</title>
+
+	<meta name="description" content="Online Job Management / Job Portal" />
+	<meta name="keywords" content="job, work, resume, applicants, application, employee, employer, hire, hiring, human resource management, hr, online job management, company, worker, career, recruiting, recruitment" />
+	<meta name="author" content="BwireSoft">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta property="og:image" content="http://<?php echo "$actual_link"; ?>/images/banner.jpg" />
+    <meta property="og:image:secure_url" content="https://<?php echo "$actual_link"; ?>/images/banner.jpg" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="500" />
+    <meta property="og:image:height" content="300" />
+    <meta property="og:image:alt" content="Nightingale Jobs" />
+    <meta property="og:description" content="Online Job Management / Job Portal" /><link rel="shortcut icon" href="images/ico/favicon.png">
+
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" media="screen">	
+	<link href="css/animate.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/component.css" rel="stylesheet">
+
+
+	<link href="css/style.css" rel="stylesheet">
+ <script type="text/javascript">
+   function update(str)
+   {
+
+	if(document.getElementById('mymail').value == "")
+   {
+	alert("Please enter your email");
+
+    }else{
+		  document.getElementById("data").innerHTML = "Please wait...";
+      var xmlhttp;
+
+      if (window.XMLHttpRequest)
+      {
+        xmlhttp=new XMLHttpRequest();
+      }
+      else
+      {
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+      }
+
+      xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200)
+        {
+          document.getElementById("data").innerHTML = xmlhttp.responseText;
+        }
+      }
+
+      xmlhttp.open("GET","app/reset-pw.php?opt="+str, true);
+      xmlhttp.send();
+}
+
+  }
+
+   function reset_text()
+   {
+   document.getElementById('mymail').value = "";
+   document.getElementById("data").innerHTML = "";
+   }
+   //------------------------------------------------------------------------------
+function validateForm() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+
+    if (username === "") {
+        alert("Please enter your name.");
+    }
+
+    let mail=document.getElementById("email").value;
+	R3=/^[^\s@]+[@][a-z]+[.][a-z]+$/;
+	if(R3.test(mail))
+	{
+
+	}
+	else
+	{
+		alert("Invalid email format");
+	}
+
+    if (password === "") {
+        alert("Password is required");
+    }
+
+    if (confirmPassword === "") {
+        alert("Confirm Password is required");
+    } else if (password !== confirmPassword) {
+        alert("Passwords do not match");
+    }
+
+     else if (R3.test(mail) && password === confirmPassword) {
+        alert("Registration successful!");
+    }
+}
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+</script>
+</head>
+
+
+<body class="not-transparent-header">
+
+	<div class="container-wrapper">
+
+
+		<header id="header">
+
+
+			<nav class="navbar navbar-default navbar-fixed-top navbar-sticky-function">
+
+				<div class="container">
+					
+					<div class="logo-wrapper">
+						<div class="logo">
+							<a href="./"><img src="images/55.png" alt="Logo" /></a>
+						</div>
+					</div>
+					
+					<div id="navbar" class="navbar-nav-wrapper navbar-arrow">
+					
+						<ul class="nav navbar-nav" id="responsive-menu">
+						
+							<li>
+							
+								<a href="./">Home</a>
+								
+							</li>
+							
+							<li>
+								<a href="job-list.php">Job List</a>
+
+							</li>
+							
+							<li>
+								<a href="employers.php">Employers</a>
+							</li>
+							
+							<li>
+								<a href="employees.php">Employees</a>
+							</li>
+							
+							<li>
+								<a href="contact.php">Contact Us</a>
+							</li>
+
+						</ul>
+				
+					</div>
+
+					<div class="nav-mini-wrapper">
+						<ul class="nav-mini sign-in">
+							<li><a  href="login.php">login</a></li>
+							<li><a data-toggle="modal" href="#registerModal">register</a></li>
+						</ul>
+					</div>
+				
+				</div>
+				
+				<div id="slicknav-mobile"></div>
+				
+			</nav>
+	
+			<div id="registerModal" class="modal fade login-box-wrapper" tabindex="-1" style="display: none;" data-backdrop="static" data-keyboard="false" data-replace="true">
+			
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title text-center">Create your account </h4>
+				</div>
+				
+				<div class="modal-body">
+				
+					<div class="row gap-20">
+					
+						<div class="col-sm-6 col-md-6">
+							<a href="register.php?p=Employer" class="btn btn-facebook btn-block mb-5-xs">Register as Employer</a>
+						</div>
+						<div class="col-sm-6 col-md-6">
+							<a href="register.php?p=Employee" class="btn btn-facebook btn-block mb-5-xs">Register as Employee</a>
+						</div>
+
+					</div>
+				
+				</div>
+				
+				<div class="modal-footer text-center">
+					<button type="button" data-dismiss="modal" class="btn btn-primary btn-inverse">Close</button>
+				</div>
+				
+			</div>
+
+
+
+			
+		</header>
+
+
+
+
+
+			<div class="login-container-wrapper">	
+	
+				<div class="container">
+				
+					<div class="row">
+					
+						<div class="col-md-10 col-md-offset-1">
+						
+							<div class="row">
+
+								<div class="col-sm-6 col-sm-offset-3">
+                                <?php
+								include 'constants/check_reply.php';	
+								?>
+                                <form name="frm" action="app/auth.php" method="POST" autocomplete="off">
+                                <div class="login-box-wrapper">
+							
+                                <div class="modal-header">
+                                <h4 class="modal-title text-center">Access your account</h4>
+                                </div>
+
+                                <div class="modal-body">
+																
+                                <div class="row gap-20">
+
+												
+                                <div class="col-sm-12 col-md-12">
+
+                                <div class="form-group"> 
+                                <label>Email Address</label>
+                                <input class="form-control" placeholder="Enter your email address" name="email" required type="text">
+                                </div>
+												
+                                 </div>
+												
+                                <div class="col-sm-12 col-md-12">
+												
+                                <div class="form-group"> 
+                                <label>Password</label>
+                                <input class="form-control" placeholder="Enter your password" name="password" required type="password">
+                                </div>
+												
+                                </div>
+
+
+						
+
+
+											
+												
+												
+</div>
+
+</div>
+
+<div class="modal-footer text-center">
+<button type="submit" class="btn btn-primary">Login</button>
+</div>
+										
+</div>
+</form>
+
+							  			<div id="forgotPasswordModal" class="modal fade login-box-wrapper" tabindex="-1" style="display: none;" data-backdrop="static" data-keyboard="false" data-replace="true">
+			
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title text-center">Restore your forgotten password</h4>
+				</div>
+
+				<div class="modal-body">
+					<div class="row gap-20">
+
+						<div class="col-sm-12 col-md-12">
+				
+							<div class="form-group"> 
+
+								<label>Email Address</label>
+								<input id="mymail" autocomplete="off" name="email" class="form-control" placeholder="Enter your email address" type="email" required> 
+							</div>
+						
+						</div>
+
+
+						
+						<div class="col-sm-12 col-md-12">
+							<div class="login-box-box-action">
+								Return to <a data-dismiss="modal">Log-in</a>
+								<p id="data"></p>
+							</div>
+							
+						</div>
+						
+					</div>
+				</div>
+				
+				<div class="modal-footer text-center">
+					<button  onclick="update(mymail.value)" type="submit" class="btn btn-primary">Restore</button>
+					<button type="button" data-dismiss="modal" class="btn btn-primary btn-inverse">Close</button>
+				</div>
+		
+			</div>
+									
+								</div>
+							
+							</div>
+							
+						</div>
+						
+					</div>
+					
+				</div>
+			
+			</div>
+			<footer class="footer-wrapper">
+			
+				<div class="main-footer">
+				
+					<div class="container">
+					
+						<div class="row">
+						
+							<div class="col-sm-12 col-md-9">
+							
+								<div class="row">
+								
+									<div class="col-sm-6 col-md-4">
+									
+										<div class="footer-about-us">
+											<h5 class="footer-title">About Jobify</h5>
+											<p>Jobify is a job portal, online job management system.</p>
+
+										</div>
+
+									</div>
+
+									<div class="col-sm-6 col-md-5 mt-30-xs">
+										<h5 class="footer-title">Quick Links</h5>
+										<ul class="footer-menu clearfix">
+											<li><a href="./">Home</a></li>
+											<li><a href="job-list.php">Job List</a></li>
+											<li><a href="employers.php">Employers</a></li>
+											<li><a href="employees.php">Employees</a></li>
+											<li><a href="contact.php">Contact Us</a></li>
+											<li><a href="#">Go to top</a></li>
+
+										</ul>
+
+									</div>
+
+								</div>
+
+							</div>
+
+							<div class="col-sm-12 col-md-3 mt-30-sm">
+
+								<h5 class="footer-title">Jobify</h5>
+
+								<p>Address : Sanjivani, Kopargaon</p>
+								<p>Email : jobify@gmail.com </p>
+								<p>Phone : 1234567890</p>
+
+
+							</div>
+
+
+						</div>
+
+					</div>
+
+				</div>
+
+				<div class="bottom-footer">
+
+					<div class="container">
+
+						<div class="row">
+
+							<div class="col-sm-4 col-md-4">
+
+								<p class="copy-right">&#169; Copyright <?php echo date('Y'); ?> Jobify</p>
+
+							</div>
+
+							<div class="col-sm-4 col-md-4">
+
+								<ul class="bottom-footer-menu">
+									<li><a >Developed by Group id 15</a></li>
+								</ul>
+
+							</div>
+
+							<div class="col-sm-4 col-md-4">
+								<ul class="bottom-footer-menu for-social">
+									<li><a href="<?php echo "$tw"; ?>"><i class="ri ri-twitter" data-toggle="tooltip" data-placement="top" title="twitter"></i></a></li>
+									<li><a href="<?php echo "$fb"; ?>"><i class="ri ri-facebook" data-toggle="tooltip" data-placement="top" title="facebook"></i></a></li>
+									<li><a href="<?php echo "$ig"; ?>"><i class="ri ri-instagram" data-toggle="tooltip" data-placement="top" title="instagram"></i></a></li>
+								</ul>
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</footer>
+			
+		</div>
+
+
+	</div> 
+
+
+<div id="back-to-top">
+   <a href="#"><i class="ion-ios-arrow-up"></i></a>
+</div>
+
+
+</body>
+
+</html>
